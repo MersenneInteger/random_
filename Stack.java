a7b9759c552611e6fdaff3cc910a0a2056360049
MersenneInteger <carlyle@Imperium.attlocal.net>
2016-06-22 16:46:50 -0500

    basic stack

---
+++ Stack.java
@@ -0,0 +1,11 @@
+public interface Stack<E>{
+
+    int size();
+    boolean isEmpty();
+    void push(E e);
+    E top();
+    E pop();
+                                                                                
+                                                                                
+}
+               
